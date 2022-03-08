d7bdf484525ffe7268070aea533c9d7578840fd2
Abid Khan <akhan@douglas.co.us>
2022-03-07 17:00:58 -0700

    adding stirng line

--- SampleNet6Api/Controllers/ElectionsController.cs
+++ SampleNet6Api/Controllers/ElectionsController.cs
@@ -26,7 +26,7 @@ namespace SampleNet6Api.Controllers
         public async Task<ActionResult<IEnumerable<Election>>> GetElections([FromQuery] PaginationParameter pp)
         {
             LogReporter lr = new LogReporter(_logger, "GetElections");
-            _logger.LogInformation("Class name from logger " + _logger.GetType().Name);
+            _logger.LogInformation("Good class name from logger " + _logger.ToString());
             PagedList<Election> elections = await PagedList<Election>.ToPagedList(_context.Elections , pp);
 
             Response.Headers.Add("dc-pagination", elections.GetMetaData());
